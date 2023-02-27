0d0d299bb6a09ec172dc3a4086a2194fdfaff0e7
fabloy <fabioloyacono.communication@gmail.com>
2023-02-27 09:50:44 +0100

    risoluzione problema comma con join("")

--- script/view/showautocomplete.js
+++ script/view/showautocomplete.js
@@ -6,21 +6,22 @@ import { name } from "../control/variables.js"
 export let showAutocomplete = (elementHTML, filmsFind)=>{
  //azione di modifica del DOM con inserimento dei titoli dei film all'interno dell'autocomplete.
  filmsFind!==undefined ? elementHTML.innerHTML = filmsFind?.map(el=>{
+
   return `
           <li 
            class="autocompleteElement" 
            code=${el.imdbID}
            >
            <p> ${el.Title}</p>
-           
            <img src=${el.Poster}  width="100" height="100">
           </li>
          `
-    })
+    }).join("")
     :
-    elementHTML.innerHTML=""
+    elementHTML.innerHTML=elementHTML.innerHTML
     name.value.length === 0 ? elementHTML.innerHTML = "" : elementHTML.innerHTML=elementHTML.innerHTML
-}
+   
+  }
 
 
 
